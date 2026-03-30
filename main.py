7b3e48fe9e8d8f0dba0c9a5dd4c6ba1fdf4f3eb1
Mateusz Krajewski <mkrajew3@gmail.com>
2026-03-30 22:37:19 +0200

    process pdfs notebook - loading file using unstructured

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+"""
+Demo quickstart sample from the Unstructured library docs.
+"""
+
+from unstructured.partition.pdf import partition_pdf
+from unstructured.staging.base import elements_to_json
+
+file_path = "data"
+base_file_name = "layout-parser-paper"
+
+
 def main():
-    print("Hello from nlp-rag!")
+    elements = partition_pdf(filename=f"{file_path}/{base_file_name}.pdf")
+    elements_to_json(
+        elements=elements, filename=f"{file_path}/{base_file_name}-output.json"
+    )
 
 
 if __name__ == "__main__":
